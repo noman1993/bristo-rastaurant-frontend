6cd6563a788153a599120f40868d1c8d2c275f05
noman1993 <chopolnoman07@gmail.com>
2025-04-24 16:37:59 +0600

    complete the testimonials section

--- src/pages/Home/PopularMenu/PopularMenu.jsx
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,31 +1,36 @@
-import React, { useEffect, useState } from 'react'
-import SectionTitle from '../../../components/SectionTitle/SectionTitle'
-import MenuItem from '../../Shared/MenuItem/MenuItem'
-
+import React, { useEffect, useState } from "react";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import MenuItem from "../../Shared/MenuItem/MenuItem";
 
 const PopularMenu = () => {
+  const [menu, setMenu] = useState([]);
 
-    const [menu, setMenu] = useState([])
-
-    useEffect(()=>{
-        fetch('menu.json')
-        .then(res => res.json())
-        .then(data => {
-            const popularItems = data.filter(item => item.category === 'popular')
-            setMenu(popularItems)
-        })
-    },[])
+  useEffect(() => {
+    fetch("menu.json")
+      .then((res) => res.json())
+      .then((data) => {
+        const popularItems = data.filter((item) => item.category === "popular");
+        setMenu(popularItems);
+      });
+  }, []);
   return (
-    <section className='mb-12'>
-      <SectionTitle heading={"From Our Menu"} subHeading={"Popular items"}></SectionTitle>
-      <div className='grid md:grid-cols-2 gap-4'>
-        {
-            menu.map(item => <MenuItem key={item._id} item={item}></MenuItem>)
-        }
+    <section className="mb-12">
+      <SectionTitle
+        heading={"From Our Menu"}
+        subHeading={"Popular items"}
+      ></SectionTitle>
+      <div className="grid md:grid-cols-2 gap-4">
+        {menu.map((item) => (
+          <MenuItem key={item._id} item={item}></MenuItem>
+        ))}
+      </div>
+      <div className="flex justify-center my-4">
+        <button className="btn btn-soft border-0 border-b-2 border-amber-300">
+          View Full Menu
+        </button>
       </div>
     </section>
-    
-  )
-}
+  );
+};
 
-export default PopularMenu
+export default PopularMenu;
---
+++ src/pages/Home/Testimonials/Testimonials.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+
+// import required modules
+import { Navigation } from "swiper/modules";
+import { Rating } from "@smastrom/react-rating";
+
+import "@smastrom/react-rating/style.css";
+
+const Testimonials = () => {
+  const [reviews, setReviews] = useState([]);
+  useEffect(() => {
+    fetch("reviews.json")
+      .then((res) => res.json())
+      .then((data) => setReviews(data));
+  }, []);
+  return (
+    <section>
+      <SectionTitle
+        heading={"testimonials"}
+        subHeading={"what our client say"}
+      ></SectionTitle>
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+        {reviews.map((review) => (
+          <SwiperSlide key={review._id}>
+            <div className=" flex flex-col items-center m-16 mx-24">
+              <Rating style={{ maxWidth: 180 }} value={review.rating} readOnly />
+              <p className="py-8">{review.details}</p>
+              <h3 className="text-2xl text-orange-400">{review.name}</h3>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </section>
+  );
+};
+
+export default Testimonials;
