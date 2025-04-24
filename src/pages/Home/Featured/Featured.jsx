b5fea05332484924bb3669b7a7ecb25edb2a40fa
noman1993 <chopolnoman07@gmail.com>
2025-04-24 15:48:40 +0600

    complete the featured UI

---
+++ src/pages/Home/Featured/Featured.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import SectionTitle from '../../../components/SectionTitle/SectionTitle'
+import featured from '../../../assets/home/featured.jpg'
+import './Featured.css'
+
+const Featured = () => {
+  return (
+    <div className='featured-item'>
+      <SectionTitle heading={"featured item"} subHeading={"Chack It Out"}></SectionTitle>
+      <div className='md:flex md:space-x-4 justify-center items-center py-10 px-24'>
+        <div>
+            <img src={featured} alt="" />
+        </div>
+        <div className='text-gray-700'>
+            <p>Aug,20,2029</p>
+            <p className='uppercase'>Where can i get some?</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero corrupti nulla reprehenderit? Nam consequatur recusandae ea atque, placeat doloremque harum molestias eos aperiam! Ipsam, unde? Mollitia blanditiis sequi iusto veritatis recusandae nostrum sapiente exercitationem nulla aliquid beatae voluptatum aut neque ex, numquam facilis saepe dolorum aspernatur sit odio magnam consectetur?</p>
+            <button className="btn btn-soft my-4 border-b-2 border-amber-300">Order Now</button>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Featured
